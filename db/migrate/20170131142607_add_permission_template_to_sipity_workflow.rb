d5b405407cac0aad76817ef681ad5dc9af1ea7d3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-02-07 10:14:36 -0500

    Adding PermissionTemplate.has_many :workflows
    This is a concession that each AdminSet has many workflows, but given
    that AdminSet is an ActiveFedora::Base and Sipity::Workflows are an
    ActiveRecord::Base, we can't cross the streams. So instead, I'm
    leveraging the fact that each AdminSet has one and only one Permission
    Template and each Permission Template belongs to one and only one
    AdminSet.

--- db/migrate/20170131142607_add_permission_template_to_sipity_workflow.rb
+++ db/migrate/20170131142607_add_permission_template_to_sipity_workflow.rb
@@ -1,6 +1,5 @@
 class AddPermissionTemplateToSipityWorkflow < ActiveRecord::Migration
   def change
-    add_column :sipity_workflows, :permission_template_id, :integer, null: false
-    add_index :sipity_workflows, :permission_template_id
+    add_column :sipity_workflows, :permission_template_id, :integer, index: true
   end
 end
